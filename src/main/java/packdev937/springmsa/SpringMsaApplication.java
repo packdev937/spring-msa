ec48514cfedbad1d8f76b367cfe589018e2ffe81
Geonwoo Pack <packdev937@gmail.com>
2024-06-17 09:21:28 +0900

    initial commit

---
+++ src/main/java/packdev937/springmsa/SpringMsaApplication.java
@@ -0,0 +1,13 @@
+package packdev937.springmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/packdev937/springmsa/SpringMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package packdev937.springmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
