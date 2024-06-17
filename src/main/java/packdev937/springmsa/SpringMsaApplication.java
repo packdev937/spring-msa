8a29996e16d2b6a4988e0782d9829c26c7151581
Geonwoo Pack <packdev937@gmail.com>
2024-06-17 09:27:00 +0900

    feat : @EnableEurekaServer 어노테이션을 추가한다 - 어플리케이션이 Service Discovery로서 동작한다

--- src/main/java/packdev937/springmsa/SpringMsaApplication.java
+++ src/main/java/packdev937/springmsa/SpringMsaApplication.java
@@ -2,8 +2,10 @@ package packdev937.springmsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringMsaApplication {
 
     public static void main(String[] args) {
