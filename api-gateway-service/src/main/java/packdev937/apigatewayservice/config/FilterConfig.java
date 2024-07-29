4787208af829aa0b25c424487bc95dbc116e7592
Geonwoo Pack <packdev937@gmail.com>
2024-07-29 14:59:36 +0900

    feat : 자바 코드로 Gateway Filter를 적용한다

--- api-gateway-service/src/main/java/packdev937/apigatewayservice/config/FilterConfig.java
+++ api-gateway-service/src/main/java/packdev937/apigatewayservice/config/FilterConfig.java
@@ -5,13 +5,15 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
 
-    @Bean
+//    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
             .route(r -> r.path("/first-service/**")
+//                .filters(f -> f.addRequestHeader("","")
+//                    .addResponseHeader("",""))
 	.uri("http://localhost:8081"))
             .route(r -> r.path("/second-service/**")
 	.uri("http://localhost:8082"))
